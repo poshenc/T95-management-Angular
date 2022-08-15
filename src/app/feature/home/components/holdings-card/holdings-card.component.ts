fcb5388bbfbda50f69b3c01e6ed47a2a41ecf9c8
johnson <poshenc@gmail.com>
2022-08-15 23:31:00 +0800

    holdings card UI

---
+++ src/app/feature/home/components/holdings-card/holdings-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-holdings-card',
+  templateUrl: './holdings-card.component.html',
+  styleUrls: ['./holdings-card.component.scss']
+})
+export class HoldingsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
