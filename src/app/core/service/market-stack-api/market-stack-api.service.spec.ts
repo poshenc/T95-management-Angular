04af79ac99e1084f69645e2064e97fac2b6b728c
johnson <poshenc@gmail.com>
2022-08-27 22:35:41 +0800

    [unit test]fix jasmine tests

--- src/app/core/service/market-stack-api/market-stack-api.service.spec.ts
+++ src/app/core/service/market-stack-api/market-stack-api.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { MarketStackApiService } from './market-stack-api.service';
@@ -6,7 +7,11 @@ describe('MarketStackApiService', () => {
   let service: MarketStackApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+      ]
+    });
     service = TestBed.inject(MarketStackApiService);
   });
 
