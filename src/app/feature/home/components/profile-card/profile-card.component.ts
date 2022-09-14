f30675820bd37b75cd591e9a4a7de02558343611
johnson <poshenc@gmail.com>
2022-09-14 23:53:36 +0800

    [logout] add: logout feature in home page

--- src/app/feature/home/components/profile-card/profile-card.component.ts
+++ src/app/feature/home/components/profile-card/profile-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-card',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    sessionStorage.clear();
+    this.router.navigateByUrl('login');
+  }
 }
