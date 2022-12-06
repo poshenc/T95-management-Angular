2d61f0b886fce7180d409c571540b375cd34c1af
Johnson <poshenc@gmail.com>
2022-12-06 15:25:48 +0800

    [portfolio card] change UI of portfolio items

--- src/app/shared/directives/collapse.directive.ts
+++ src/app/shared/directives/collapse.directive.ts
@@ -1,19 +1,27 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, AUTO_STYLE, state, style, transition, trigger } from '@angular/animations';
 import { Component, HostBinding, HostListener, Input } from '@angular/core';
 
 @Component({
   selector: '[collapse]',
   template: '<ng-content></ng-content>',
+  // animations: [
+  //   trigger('collapse', [
+  //     state('0', style({ height: '0', opacity: '0' })),
+  //     state('1', style({ height: '*', opacity: '1' })),
+  //     transition('0 <=> 1', animate('{{duration}}ms {{easing}}'), {
+  //       params: {
+  //         duration: 200,
+  //         easing: "ease-in-out"
+  //       }
+  //     })
+  //   ])
+  // ]
   animations: [
     trigger('collapse', [
-      state('0', style({ height: '0', opacity: '0' })),
-      state('1', style({ height: '*', opacity: '1' })),
-      transition('0 <=> 1', animate('{{duration}}ms {{easing}}'), {
-        params: {
-          duration: 500,
-          easing: "ease-in-out"
-        }
-      })
+      state('true', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+      state('false', style({ height: '0', visibility: 'hidden' })),
+      transition('false => true', animate(300 + 'ms ease-in')),
+      transition('true => false', animate(300 + 'ms ease-out'))
     ])
   ]
 })
