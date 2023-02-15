effe8846d68758d3e81dde7ff2135421b56dd708
Johnson <poshenc@gmail.com>
2023-02-15 17:03:10 +0800

    [home page] add: websocket market data for key index

--- src/app/feature/home/pages/home.component.ts
+++ src/app/feature/home/pages/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { SessionsService } from 'src/app/core/service/sessions/sessions.service';
 import { StocksService } from 'src/app/core/service/stocks/stocks.service';
-import { portfolioCard, priceCard } from 'src/app/feature/home/models/price-card.models';
+import { priceCard } from 'src/app/feature/home/models/price-card.models';
 import { RxStompService } from '../../portfolio/services/webSocket/rx-stomp.service';
 import { stompWebSocketConfig } from '../../portfolio/services/webSocket/stompWebSocket.config';
 
@@ -14,9 +14,6 @@ import { stompWebSocketConfig } from '../../portfolio/services/webSocket/stompWe
 export class HomeComponent implements OnInit {
 
   stockData = [] as priceCard[];
-  stockList: string[];
-  portfolioData: portfolioCard[];
-
   //props to children
   public showMoney = true;
 
@@ -27,40 +24,10 @@ export class HomeComponent implements OnInit {
   private downloadStatusSubscription: Subscription;
 
   constructor(private stocksService: StocksService, private sessionsService: SessionsService, private rxStompService: RxStompService) {
-
-    //for fetch API use
-    this.stockList = ['AAPL', 'TSLA']
-
-    //for portfolio
-    this.portfolioData = [
-      {
-        portfolioName: 'US Stock',
-        total: 120621.92,
-        movementAmount: 39.66,
-        movementPercentage: 0.04
-      },
-      {
-        portfolioName: 'TW Stock',
-        total: 27288.08,
-        movementAmount: 118.84,
-        movementPercentage: 0.44
-      },
-      {
-        portfolioName: 'Crypto',
-        total: 24240.16,
-        movementAmount: -95.11,
-        movementPercentage: -0.39
-      },
-    ]
-
   }
 
-
-
   ngOnInit(): void {
-    //yahoo finance API
-    // this.getIntradayPriceData(this.stockList);
-    // this.fetchStocks();
+    this.fetchStocks();
     //websocket
     this.connectWebsocket();
     this.subscribeWebsocket();
@@ -70,7 +37,15 @@ export class HomeComponent implements OnInit {
     this.downloadStatusSubscription?.unsubscribe();
   }
 
-  //websocket maert data status
+  async fetchStocks() {
+    const mainSymbols = ['DOW J', 'IXIC', 'GSPC', 'USDTWD', 'TSLA', 'APPL', '2330', 'BTC', 'ETH']
+    for (let symbol of mainSymbols) {
+      const res = await lastValueFrom(this.stocksService.getStockBySymbol(symbol));
+      this.stockData.push(res);
+    }
+  }
+
+  //websocket market data status
   connectWebsocket() {
     // const currentUrl = this.appConfigService.settings.apiServer.url;
     // downloadStatusConfig.brokerURL = 'ws' + currentUrl.slice(currentUrl.indexOf(':')) + 'websocket';
@@ -86,32 +61,8 @@ export class HomeComponent implements OnInit {
     this.downloadStatusSubscription = this.rxStompService
       .watch('/topic/globalIndex', stompWebSocketConfig.connectHeaders)
       .subscribe((data: any) => {
-        console.log('incoming message', data);
-
-        // const message = JSON.parse(data.body);
-        // console.log('incoming message', message);
-        // this.marketData.push(message);
-
+        this.stockData = JSON.parse(data.body);
+        // console.log('Scheduled update stock data,', this.stockData);
       });
   }
-
-  // getIntradayPriceData(stockList: string[]) {
-  //   stockList.map(async stock => {
-  //     let data = await this.yahooFinanceApiService.getIntradayPriceData(stock).toPromise();
-  //     console.log('stock name:', stock)
-  //     console.log('regularMarketPrice', data.quoteResponse.result[0].regularMarketPrice);
-  //     console.log('regularMarketChange', data.quoteResponse.result[0].regularMarketChange)
-  //     console.log('regularMarketChangePercent', data.quoteResponse.result[0].regularMarketChangePercent);
-  //   })
-  // }
-
-  async fetchStocks() {
-    const mainSymbols = ['DOW J', 'IXIC', 'GSPC', 'USDTWD', 'TSLA', 'APPL', '2330', 'BTC', 'ETH']
-    for (let symbol of mainSymbols) {
-      const res = await lastValueFrom(this.stocksService.getStockBySymbol(symbol));
-      this.stockData.push(res);
-    }
-  }
-
-
 }
